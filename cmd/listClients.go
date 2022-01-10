ef793bf1fe34aaa2cf5c28b0e96203e783d92d6c
Miguel Tenorio <mtpotro41@gmail.com>
2022-01-09 23:12:27 -0500

    feat: Add listClients cmd to show the supported clients

---
+++ cmd/listClients.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2022 Nethermind hello.nethermind.io
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/NethermindEth/1Click/configs"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// listClientsCmd represents the listClients command
+var listClientsCmd = &cobra.Command{
+	Use:   "listClients",
+	Short: "List supported clients",
+	Long:  `List supported clients for execution and consensus engines`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info("Listing supported clients")
+		fmt.Printf("Eth1 Clients: \n%s\n\n", strings.Join(configs.Eth1ClientsSupported, ", "))
+		fmt.Printf("Consensus Clients: \n%s\n", strings.Join(configs.ConsensusClientsSupported, ", "))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listClientsCmd)
+}
