525cca385e94a3010f11229f622783f0796edc1c
Miguel Tenorio <mtpotro41@gmail.com>
2022-01-09 23:14:34 -0500

    feat: Add bash scripts for linux instructions for docker setup

---
+++ scripts/setup/linux/docker-compose/amzn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install docker-compose
+
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
---
+++ scripts/setup/linux/docker/amzn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install docker
+
+sudo yum -y update
+sudo yum -y install docker
