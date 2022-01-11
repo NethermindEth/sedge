f31b8572af368ec9c6ed846001e5e063857badd2
Miguel Tenorio <mtpotro41@gmail.com>
2022-01-10 22:19:36 -0500

    refac: InstallDependencies functions of utils

--- internal/utils/install_dependencies.go
+++ internal/utils/install_dependencies.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"runtime"
 
 	log "github.com/sirupsen/logrus"
@@ -10,9 +11,9 @@ import (
 func InstallDependencies(dependencies []string) error {
 	switch os := runtime.GOOS; os {
 	case "linux":
-		log.Error("Dependencies are not installed on your machine. Please install them and try again.")
+		return fmt.Errorf("dependencies are not installed on your machine. Please install them and try again")
 	case "windows":
-		log.Error("Dependencies are not installed on your machine. Please install them and try again.")
+		return fmt.Errorf("dependencies are not installed on your machine. Please install them and try again")
 	default:
 		log.Fatalf("Dependencies %s are not installed on your machine. Please install them and try again.", dependencies)
 	}
