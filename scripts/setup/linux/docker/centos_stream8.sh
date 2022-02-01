0eda08eae86de92d63be290d00d1297a436c7de0
Nick <49295025+nickkjolsing@users.noreply.github.com>
2022-02-01 13:28:55 -0600

    Added CentOS Steam 8 docker and docker-compose install scripts

--- scripts/setup/linux/docker/centos_stream8.sh
+++ scripts/setup/linux/docker/centos_stream8.sh
@@ -7,3 +7,5 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
