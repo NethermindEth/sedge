9474e82958a7773e5da1b552ed005122ef2c9994
Miguel Tenorio <mtpotro41@gmail.com>
2022-02-03 10:17:38 -0500

    doc: Add comments and logs to RunDockerCompose function

--- internal/utils/run_scripts.go
+++ internal/utils/run_scripts.go
@@ -3,11 +3,26 @@ package utils
 import (
 	"fmt"
 	"text/template"
+
+	"github.com/NethermindEth/1Click/configs"
+	log "github.com/sirupsen/logrus"
 )
 
-// Run docker-compose scripts
+/*
+RunDockerCompose :
+This function is responsible for running the generated doccker-compose script.
+
+params :-
+a. path string
+Path of generated script
+
+returns :-
+a. error
+Error if any
+*/
 func RunDockerCompose(path string) error {
-	cmd := fmt.Sprintf("sudo docker-compose -f %s up -d", path)
+	log.Info(configs.RunningDockerCompose)
+	cmd := fmt.Sprintf(configs.DockerComposeCMD, path)
 	tmp, err := template.New("script").Parse(cmd)
 	if err != nil {
 		return err
