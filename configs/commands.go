fd1597e543fe0bb72529c76f6ac8c0cf0fb84f26
Miguel Tenorio <mtpotro41@gmail.com>
2022-03-09 21:54:53 -0500

    fix: Remove quotes from DockerComposePsServicesCMD

--- configs/commands.go
+++ configs/commands.go
@@ -4,7 +4,7 @@ package configs
 const (
 	DockerComposeUpCMD         = "docker-compose -f %s up -d execution consensus"
 	DockerPsCMD                = "docker ps -a"
-	DockerComposePsServicesCMD = "docker-compose -f %s ps --services --filter \"status=running\""
+	DockerComposePsServicesCMD = "docker-compose -f %s ps --services --filter status=running"
 	DockerComposeLogsFollowCMD = "docker-compose -f %s logs --follow %s"
 	DockerComposeLogsTailCMD   = "docker-compose -f %s logs --tail=20 %s"
 	DepositCLIDockerBuildCMD   = "docker build github.com/ethereum/eth2.0-deposit-cli -t %s"
