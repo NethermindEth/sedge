eeb09d99ec2f8a07c22e97816e337bcbc5b0bd99
Carlos Bermudez <cbermudez.dev@gmail.com>
2022-04-04 17:38:35 -0400

    test: implement unit test for contains helper

---
+++ internal/utils/utils_test.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestContains(t *testing.T) {
+	inputs := []struct {
+		list []string
+		str  string
+		want bool
+	}{
+		{},
+		{[]string{"a", "A", "b", "B"}, "b", true},
+		{[]string{"a", "A", "b", "B"}, "c", false},
+		{[]string{}, "a", false},
+	}
+
+	for _, input := range inputs {
+		descr := fmt.Sprintf("Contains(%s)", input.list)
+		if got := Contains(input.list, input.str); got != input.want {
+			t.Errorf("%s expected %t but got %t", descr, input.want, got)
+		}
+	}
+}
