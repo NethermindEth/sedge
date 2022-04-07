7e7280a561eba8e1d1359ed6c52951ee1e12c703
Carlos Bermudez <cbermudez.dev@gmail.com>
2022-04-07 12:56:16 -0400

    test: write skip lines test

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -5,6 +5,29 @@ import (
 	"testing"
 )
 
+func TestSkipLines(t *testing.T) {
+	inputs := []struct {
+		content string
+		symbol  string
+		result  string
+	}{
+		{},
+		{"bbbb", "a", "bbbb"},
+		{"aaaa", "a", ""},
+		{"bbbb\naaaa", "a", "bbbb"},
+		{"aaaa\nbbbb", "a", "bbbb"},
+	}
+
+	for _, input := range inputs {
+		descr := fmt.Sprintf("SkipLines(%s, %s)", input.content, input.symbol)
+		got := SkipLines(input.content, input.symbol)
+
+		if got != input.result {
+			t.Errorf("%s expected %s but got %s", descr, input.result, got)
+		}
+	}
+}
+
 func TestContains(t *testing.T) {
 	inputs := [...]struct {
 		list []string
