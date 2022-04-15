c5d84ea260ccfd4ea214ca16879b66e44b8fa03c
Carlos Bermudez <cbermudez.dev@gmail.com>
2022-04-15 09:57:29 -0400

    test: write test for check containers util method

--- internal/utils/checks_test.go
+++ internal/utils/checks_test.go
@@ -77,25 +77,66 @@ func TestPreCheck(t *testing.T) {
 	}
 }
 
+type checkContainersTC struct {
+	path     string
+	runner   commands.CommandRunner
+	isErr    bool
+	psRunned int
+}
+
+func buildCheckContainersTestCase(t *testing.T, caseName string, isErr bool) *checkContainersTC {
+	dcPath := t.TempDir()
+	test.PrepareTestCaseDir(filepath.Join("testdata", "checks_tests", caseName, "docker-compose-scripts"), dcPath)
+
+	tc := checkContainersTC{}
+	tc.path = dcPath
+	tc.runner = &test.SimpleCMDRunner{
+		SRunCMD: func(c commands.Command) (string, error) {
+			if strings.Contains(c.Cmd, "docker-compose") && strings.Contains(c.Cmd, "ps") {
+				tc.psRunned += 1
+				_, err := os.Lstat(filepath.Join(dcPath, configs.DefaultDockerComposeScriptName))
+				return "", err
+			}
+			return "", nil
+		},
+		SRunBash: func(bs commands.BashScript) (string, error) {
+			return "", nil
+		},
+	}
+	tc.isErr = isErr
+	return &tc
+}
+
 func TestCheckContainers(t *testing.T) {
-	//TODO: fix problems with sudo and containers
-	inputs := [...]struct {
-		path  string
-		isErr bool
-	}{
-		// {
-		// 	t.TempDir(),
-		// 	false,
-		// },
+	tcs := [...]*checkContainersTC{
+		buildCheckContainersTestCase(
+			t,
+			"case_1",
+			false,
+		),
+		buildCheckContainersTestCase(
+			t,
+			"case_2",
+			true,
+		),
 	}
 
-	for _, input := range inputs {
-		descr := fmt.Sprintf("CheckContainers(%s)", input.path)
-		_, err := CheckContainers(input.path)
-		if input.isErr && err == nil {
+	for _, tc := range tcs {
+		descr := fmt.Sprintf("CheckContainers(%s)", tc.path)
+
+		commands.InitRunner(func() commands.CommandRunner {
+			return tc.runner
+		})
+
+		_, err := CheckContainers(tc.path)
+		if tc.isErr && err == nil {
 			t.Errorf("%s expected to fail", descr)
-		} else if !input.isErr && err != nil {
-			t.Errorf("%s failed: %v", descr, err)
+		} else if !tc.isErr {
+			if err != nil {
+				t.Errorf("%s failed: %v", descr, err)
+			} else if tc.psRunned < 1 {
+				t.Errorf("%s didn't run docker-compose ps", descr)
+			}
 		}
 	}
 }
