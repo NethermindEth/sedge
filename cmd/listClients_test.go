5416a116141c1b31299b075b6bc13877f0f68d45
Carlos Bermudez <cbermudez.dev@gmail.com>
2022-04-27 19:28:08 -0400

    test: add test cases for clients command

--- cmd/listClients_test.go
+++ cmd/listClients_test.go
@@ -2,20 +2,65 @@ package cmd
 
 import (
 	"bytes"
+	"path/filepath"
 	"testing"
 
+	"github.com/NethermindEth/1click/test"
 	log "github.com/sirupsen/logrus"
 )
 
-func TestListClientsCmd(t *testing.T) {
-	fdOut := new(bytes.Buffer)
+type listClientsTestCase struct {
+	name       string
+	logsOut    *bytes.Buffer
+	tableOut   *bytes.Buffer
+	configPath string
+	isErr      bool
+}
 
-	rootCmd.SetArgs([]string{"clients"})
-	rootCmd.SetOut(fdOut)
-	log.SetOutput(fdOut)
+func resetListClientsCmd() {
+	cfgFile = ""
+}
 
-	err := rootCmd.Execute()
+func buildListClientTestCase(t *testing.T, name, caseTestDataDir string, isErr bool) listClientsTestCase {
+	tc := listClientsTestCase{}
+	tmpDir := t.TempDir()
+
+	err := test.PrepareTestCaseDir(filepath.Join("testdata", "list_clients_tests", caseTestDataDir, "config"), tmpDir)
 	if err != nil {
-		t.Errorf("listClientsCmd.Execute() failed: %v", err)
+		t.Fatalf("Can't build test case: %v", err)
 	}
+
+	tc.name = name
+	tc.configPath = filepath.Join(tmpDir, "config.yaml")
+	tc.logsOut = new(bytes.Buffer)
+	tc.tableOut = new(bytes.Buffer)
+	tc.isErr = isErr
+	return tc
+}
+
+func TestListClientsCmd(t *testing.T) {
+	tcs := [...]listClientsTestCase{
+		buildListClientTestCase(t, "Ok", "case_1", false),
+		// buildListClientTestCase(t, "Missing validator clients", "case_2", true),
+		// buildListClientTestCase(t, "Invalid format", "case_3", true),
+	}
+
+	t.Cleanup(resetListClientsCmd)
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			resetListClientsCmd()
+			rootCmd.SetOut(tc.tableOut)
+			log.SetOutput(tc.logsOut)
+			rootCmd.SetArgs([]string{"clients", "--config", tc.configPath})
+
+			err := rootCmd.Execute()
+			if tc.isErr && err == nil {
+				t.Error("1click clients expected to fail")
+			} else if !tc.isErr && err != nil {
+				t.Errorf("1click clients failed: %v", err)
+			}
+		})
+	}
+
 }
