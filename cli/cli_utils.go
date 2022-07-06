7b9b1de0f9ff2a12a2aae686bdf98c6ec7d12b38
AntiD2ta <mtpotro41@gmail.com>
2022-07-06 10:03:40 -0400

    doc(cli): Change FIXME for normal comment

--- cli/cli_utils.go
+++ cli/cli_utils.go
@@ -297,7 +297,7 @@ func trackSync(m MonitoringTool, elPort, clPort string, wait time.Duration) erro
 			// Stop tracking
 			done <- struct{}{}
 			log.Info(configs.NodesSynced)
-			break // FIXME: statuses channel seems not being closed despite done channel signaled
+			break // statuses channel might still have data before closing done channel
 		}
 	}
 
