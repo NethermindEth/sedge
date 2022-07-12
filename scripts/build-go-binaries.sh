3b94d1a4aae77b63128ccf8080b8eddfdb7294cd
Falco <1364936+FalcoXYZ@users.noreply.github.com>
2022-07-12 11:24:13 +0200

    build multi arch binaries action
    Github action and bash script to build amd64 & arm64 binaries for Linux and Darwin

---
+++ scripts/build-go-binaries.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+package=cmd/main.go
+package_name=sedge
+	
+platforms=("linux/amd64" "linux/arm64" "darwin/amd64" "darwin/arm64")
+
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+	output_name=$package_name'-'$GOOS'-'$GOARCH
+
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o build/$output_name $package
+	if [ $? -ne 0 ]; then
+   		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+done
