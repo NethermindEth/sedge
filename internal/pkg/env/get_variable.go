fe15040334537a5bff89a1426d22678e21d9ec3c
AntiD2ta <mtpotro41@gmail.com>
2022-08-21 01:52:08 +0000

    feat: Add func to get Bootnodes from .env templates

---
+++ internal/pkg/env/get_variable.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2022 Nethermind
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package env
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/NethermindEth/sedge/configs"
+	"github.com/NethermindEth/sedge/templates"
+	log "github.com/sirupsen/logrus"
+)
+
+func GetBootnodes(network, client string) ([]string, error) {
+	content, err := templates.Envs.ReadFile(filepath.Join("envs", network, "consensus", client+".tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	if m := ReBOOTNODES.FindStringSubmatch(string(content)); m != nil {
+		m[1] = strings.ReplaceAll(m[1], "\"", "")
+		enrs := strings.Split(m[1], ",")
+		return enrs, nil
+	}
+
+	log.Warnf(configs.NoBootnodesFound, network, "consensus", client)
+	return nil, nil
+}
