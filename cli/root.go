8c7e3da7f72ab695982d13f0c2fb6cb384ca2286
adriantpaez <adriantpaez@gmail.com>
2022-11-07 16:02:36 -0500

    refactor: update 'down' command initialization

--- cli/root.go
+++ cli/root.go
@@ -46,11 +46,16 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	rootCmd.AddCommand(KeysCmd(prompts.NewPromptCli()))
+	addCommands(rootCmd)
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func addCommands(root *cobra.Command) {
+	rootCmd.AddCommand(KeysCmd(prompts.NewPromptCli()))
+	rootCmd.AddCommand(DownCmd())
 }
 
 func init() {
