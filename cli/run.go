5df69fd525d95e66515cdedac39d02a1c84be316
adriantpaez <adriantpaez@gmail.com>
2022-12-28 13:34:45 -0500

    feat: add run command

---
+++ cli/run.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"github.com/NethermindEth/sedge/cli/actions"
+	"github.com/NethermindEth/sedge/configs"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func RunCmd(sedgeActions actions.SedgeActions) *cobra.Command {
+	// Flags
+	var (
+		generationPath string
+	)
+
+	cmd := &cobra.Command{
+		Use:   "run [flags]",
+		Short: "Run services",
+		Long:  "Run all the generated services",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := sedgeActions.SetupContainers(actions.SetupContainersOptions{
+				GenerationPath: generationPath,
+			})
+			if err != nil {
+				log.Fatalf("error setting up service containers: %s", err.Error())
+			}
+			err = sedgeActions.RunContainers(actions.RunContainersOptions{
+				GenerationPath: generationPath,
+			})
+			if err != nil {
+				log.Fatalf("error starting service containers: %s", err.Error())
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&generationPath, "path", "p", configs.DefaultDockerComposeScriptsPath, "docker-compose scripts generation path")
+	return cmd
+}
--- cmd/sedge/main.go
+++ cmd/sedge/main.go
@@ -52,6 +52,7 @@ func main() {
 		cli.LogsCmd(cmdRunner),
 		cli.VersionCmd(),
 		cli.SlashingExportCmd(sedgeActions),
+		cli.RunCmd(sedgeActions),
 	)
 	if err := sedgeCmd.Execute(); err != nil {
 		os.Exit(1)
