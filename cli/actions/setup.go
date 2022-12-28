e91884023e46960e215fa7a77ea74b1e137e0a39
adriantpaez <adriantpaez@gmail.com>
2022-12-28 15:06:32 -0500

    fix: update SetupContainers docker-compose path

--- cli/actions/setup.go
+++ cli/actions/setup.go
@@ -1,6 +1,9 @@
 package actions
 
 import (
+	"path/filepath"
+
+	"github.com/NethermindEth/sedge/configs"
 	"github.com/NethermindEth/sedge/internal/pkg/commands"
 	log "github.com/sirupsen/logrus"
 )
@@ -13,21 +16,21 @@ type SetupContainersOptions struct {
 func (s *sedgeActions) SetupContainers(options SetupContainersOptions) error {
 	log.Info("Setting up containers")
 	buildCmd := s.commandRunner.BuildDockerComposeBuildCMD(commands.DockerComposeBuildOptions{
-		Path:     options.GenerationPath,
+		Path:     filepath.Join(options.GenerationPath, configs.DefaultDockerComposeScriptName),
 		Services: options.Services,
 	})
 	if _, err := s.commandRunner.RunCMD(buildCmd); err != nil {
 		return err
 	}
 	pullCmd := s.commandRunner.BuildDockerComposePullCMD(commands.DockerComposePullOptions{
-		Path:     options.GenerationPath,
+		Path:     filepath.Join(options.GenerationPath, configs.DefaultDockerComposeScriptName),
 		Services: options.Services,
 	})
 	if _, err := s.commandRunner.RunCMD(pullCmd); err != nil {
 		return err
 	}
 	createCmd := s.commandRunner.BuildDockerComposeCreateCMD(commands.DockerComposeCreateOptions{
-		Path:     options.GenerationPath,
+		Path:     filepath.Join(options.GenerationPath, configs.DefaultDockerComposeScriptName),
 		Services: options.Services,
 	})
 	if _, err := s.commandRunner.RunCMD(createCmd); err != nil {
