6a52c22e5de7cbc0864b36bdcede290a02cecb39
Hamza Shafique <hshafiq99@gmail.com>
2023-01-06 21:24:43 +0500

    Made dependency install changes

--- scripts/copy-ppa-packages-to-new-series.sh
+++ scripts/copy-ppa-packages-to-new-series.sh
@@ -3,13 +3,7 @@
 echo 'Installing Ubuntu Archive Tools'
 git clone https://git.launchpad.net/ubuntu-archive-tools
 sudo apt-get install ubuntu-dev-tools -y
-# echo 'Installing Pip'
-# sudo apt install python3-pip  -y
 cd ubuntu-archive-tools
-echo 'Installing LaunchpadLib'
-pip install launchpadlib
-echo 'Upgrading LaunchpadLib to the latest version'
-pip install launchpadlib --upgrade
 echo 'Copying Packages'
 python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=focal sedge --dry-run
 python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=kinetic sedge --dry-run
