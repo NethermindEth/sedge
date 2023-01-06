59e99e7801151c9927dd5cb55ea0dba11224566e
Hamza Shafique <hshafiq99@gmail.com>
2023-01-06 20:23:03 +0500

    Added new script for copying binaries and a new github workflow

---
+++ scripts/copy-ppa-packages-to-new-series.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+echo 'Installing Ubuntu Archive Tools'
+git clone https://git.launchpad.net/ubuntu-archive-tools
+sudo apt-get install ubuntu-dev-tools -y
+echo 'Installing Pip'
+sudo apt install python3-pip  -y
+cd ubuntu-archive-tools
+echo 'Installing LaunchpadLib'
+pip install launchpadlib
+echo 'Upgrading LaunchpadLib to the latest version'
+pip install launchpadlib --upgrade
+echo 'Copying Packages'
+python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=focal sedge --dry-run
+python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=kinetic sedge --dry-run
+python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=bionic sedge --dry-run
+python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=trusty sedge --dry-run
+cd ..
+echo 'Cleanup'
+sudo rm -rf ubuntu-archive-tools
