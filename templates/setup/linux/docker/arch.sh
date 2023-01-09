e9c79f5393affc1da343ff9b98dced2ea2ea563f
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2023-01-09 15:45:45 -0500

    feat: update docker-compose installation on arch

--- templates/setup/linux/docker/arch.sh
+++ templates/setup/linux/docker/arch.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-sudo pacman -S --noconfirm docker
+sudo pacman -S --noconfirm docker docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
-
-# Install docker-compose
-
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-$(uname -s)-$(uname -m)" -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
