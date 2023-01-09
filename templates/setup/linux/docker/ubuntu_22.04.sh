145e0aa8679747b3ea14281d968395ed5f437087
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2023-01-09 10:25:00 -0500

    feat: update install dependencies for ubuntu 22.04

--- templates/setup/linux/docker/ubuntu_22.04.sh
+++ templates/setup/linux/docker/ubuntu_22.04.sh
@@ -4,22 +4,23 @@
 sudo apt-get update
 
 sudo apt-get install -y \
-    apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    gnupg \
+    lsb-release
 
 # Add Docker’s Official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Setup Stable repo
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker
 sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Install docker-compose
 
