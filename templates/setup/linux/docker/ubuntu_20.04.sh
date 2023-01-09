a1c86364a4e75566aa8190942f03582eb5c11e91
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2023-01-09 10:41:45 -0500

    feat: format ubuntu install templates

--- templates/setup/linux/docker/ubuntu_20.04.sh
+++ templates/setup/linux/docker/ubuntu_20.04.sh
@@ -4,10 +4,10 @@
 sudo apt-get update
 
 sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
 
 # Add Docker’s Official GPG key
 sudo mkdir -p /etc/apt/keyrings
@@ -16,8 +16,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 # Setup Stable repo
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # Install Docker
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
--- templates/setup/linux/docker/ubuntu_22.04.sh
+++ templates/setup/linux/docker/ubuntu_22.04.sh
@@ -19,5 +19,5 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # Install Docker
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
