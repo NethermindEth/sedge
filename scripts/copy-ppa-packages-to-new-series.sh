15b3da33294f4e0a287b88f1c9754e771f78c02f
Hamza Shafique <hshafiq99@gmail.com>
2023-01-11 14:08:51 +0500

    Changed account name attribute

--- scripts/copy-ppa-packages-to-new-series.sh
+++ scripts/copy-ppa-packages-to-new-series.sh
@@ -5,10 +5,10 @@ git clone https://git.launchpad.net/ubuntu-archive-tools
 sudo apt-get install ubuntu-dev-tools -y
 cd ubuntu-archive-tools
 echo 'Copying Packages'
-python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=focal sedge --dry-run
-python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=kinetic sedge --dry-run
-python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=bionic sedge --dry-run
-python3 copy-package -y -b -p nethermind --ppa-name=nethermind -s jammy --to-suite=trusty sedge --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=focal sedge --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=kinetic sedge --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=bionic sedge --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=trusty sedge --dry-run
 cd ..
 echo 'Cleanup'
 sudo rm -rf ubuntu-archive-tools
