5b122e83f17426f84eee3dc061f28d1839c12146
Hamza Shafique <hshafiq99@gmail.com>
2023-01-11 14:32:35 +0500

    Updating package name

--- scripts/copy-ppa-packages-to-new-series.sh
+++ scripts/copy-ppa-packages-to-new-series.sh
@@ -5,10 +5,10 @@ git clone https://git.launchpad.net/ubuntu-archive-tools
 sudo apt-get install ubuntu-dev-tools -y
 cd ubuntu-archive-tools
 echo 'Copying Packages'
-python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=focal sedge --dry-run
-python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=kinetic sedge --dry-run
-python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=bionic sedge --dry-run
-python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=trusty sedge --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=focal nethermind --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=kinetic nethermind --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=bionic nethermind --dry-run
+python3 copy-package -y -b -p nethermindeth --ppa-name=nethermind -s jammy --to-suite=trusty nethermind --dry-run
 cd ..
 echo 'Cleanup'
 sudo rm -rf ubuntu-archive-tools
