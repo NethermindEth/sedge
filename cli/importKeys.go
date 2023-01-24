745a01cb637088dadaaed23715b8c1634fb37b97
adriantpaez <adriantpaez@gmail.com>
2023-01-24 12:05:30 +0100

    doc: update import-key command description

--- cli/importKeys.go
+++ cli/importKeys.go
@@ -1,11 +1,15 @@
 package cli
 
 import (
+	"errors"
+
 	"github.com/NethermindEth/sedge/cli/actions"
 	"github.com/NethermindEth/sedge/configs"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+var ErrInvalidNumberOfArguments = errors.New("invalid number of arguments")
 
 func ImportKeysCmd(sedgeActions actions.SedgeActions) *cobra.Command {
 	// Flags
@@ -21,9 +25,33 @@ func ImportKeysCmd(sedgeActions actions.SedgeActions) *cobra.Command {
 	)
 
 	cmd := &cobra.Command{
-		Use:   "import-key [flags]",
+		Use:   "import-key [flags] [validator]",
 		Short: "Import validator keys",
-		Long:  "Import validator keys. Validator is stopped if is currently running",
+		Long: `
+Import validator client keys, use the 'from' flag to specify the keys location,
+and make sure that follows the EIP-2335: BLS12-381 Keystore standard. This command
+assumes that the validator client container exists, stopped or not.
+
+This command stops the validator client during the importing process due to the
+validator database being locked while it's running but leaves the validator client
+in the same state in which it was found. That means if the validator is running/stopped
+before the import, then the validator will be running/stopped after the command
+is executed, regardless of whether the export fails or not. To force a different
+behavior use --start-validator and --stop-validator flags.
+
+The [validator] is a required argument used to specify which validator client, from
+all supported by Sedge (lighthouse, lodestar, prysm or teku), is used to import the
+Slashing Protection data. This is necessary because each client has its own way to
+achieve the importation.`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return ErrInvalidNumberOfArguments
+			}
+			return nil
+		},
+		PreRun: func(cmd *cobra.Command, args []string) {
+			validatorClient = args[0]
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			options := actions.ImportValidatorKeysOptions{
 				ValidatorClient: validatorClient,
@@ -48,13 +76,12 @@ func ImportKeysCmd(sedgeActions actions.SedgeActions) *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringVarP(&validatorClient, "validator", "v", "", "validator engine client")
 	cmd.Flags().StringVarP(&network, "network", "n", "", "network")
-	cmd.Flags().BoolVar(&startValidator, "start-validator", false, "if the validator client is currently stopped, then it is started after slashing export")
-	cmd.Flags().BoolVar(&stopValidator, "stop-validator", false, "if the validator client is currently running, then it is not started after slashing export")
+	cmd.Flags().BoolVar(&startValidator, "start-validator", false, "starts the validator client after import, regardless of the state the validator was in before")
+	cmd.Flags().BoolVar(&stopValidator, "stop-validator", false, "stops the validator client after import, regardless of the state the validator was in before")
 	cmd.Flags().StringVar(&from, "from", configs.DefaultDockerComposeScriptsPath, "docker-compose scripts generation path")
-	cmd.Flags().StringVar(&customConfigPath, "custom-config", "", "File path or url to use as custom network config.")
-	cmd.Flags().StringVar(&customGenesisPath, "custom-genesis", "", "File path or url to use as custom network genesis.")
-	cmd.Flags().StringVar(&customDeployBlock, "custom-deploy-block", "", "Custom network deploy block.")
+	cmd.Flags().StringVar(&customConfigPath, "custom-config", "", "file path or url to use as custom network config.")
+	cmd.Flags().StringVar(&customGenesisPath, "custom-genesis", "", "file path or url to use as custom network genesis.")
+	cmd.Flags().StringVar(&customDeployBlock, "custom-deploy-block", "", "fustom network deploy block.")
 	return cmd
 }
