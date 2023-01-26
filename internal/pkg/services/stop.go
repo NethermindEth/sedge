ab7b56fe9017d94959f5e3e1d9d44631b9a9a754
adriantpaez <adriantpaez@gmail.com>
2023-01-26 12:48:05 +0100

    chore: update stopping service log

--- internal/pkg/services/stop.go
+++ internal/pkg/services/stop.go
@@ -33,7 +33,7 @@ func (s *serviceManager) Stop(service string) error {
 		return err
 	}
 	if ctInfo.State.Running {
-		log.Infof("stopping service: %s, currently on %s status", service, ctInfo.State.Status)
+		log.Infof("Stopping service: %s, currently on %s status", service, ctInfo.State.Status)
 		timeout := 5 * time.Minute
 		if err := s.dockerClient.ContainerStop(context.Background(), ctInfo.ID, &timeout); err != nil {
 			return fmt.Errorf("%w %s: %s", ErrStoppingContainer, service, err)
