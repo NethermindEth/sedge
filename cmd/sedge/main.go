e746a954a5308e3e3939fafab7fe02f8a6771f1f
adriantpaez <adriantpaez@gmail.com>
2023-01-31 10:05:08 +0100

    feat: remove older cli command

--- cmd/sedge/main.go
+++ cmd/sedge/main.go
@@ -47,8 +47,7 @@ func main() {
 	sedgeActions := actions.NewSedgeActions(dockerClient, serviceManager, cmdRunner)
 	sedgeCmd := cli.RootCmd()
 	sedgeCmd.AddCommand(
-		cli.CliCmd(cmdRunner, prompt, serviceManager, sedgeActions),
-		cli.CliRefactorCmd(prompt, sedgeActions),
+		cli.CliCmd(prompt, sedgeActions),
 		cli.KeysCmd(cmdRunner, prompt),
 		cli.DownCmd(cmdRunner),
 		cli.ClientsCmd(),
