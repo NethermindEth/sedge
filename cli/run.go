511a1ed18f2f9730e6c8e20bb35f36c8e788ef6a
AntiD2ta <mtpotro41@gmail.com>
2023-02-17 11:08:23 +0000

    feat: Add PreRun check for run cmd

--- cli/run.go
+++ cli/run.go
@@ -16,14 +16,20 @@ limitations under the License.
 package cli
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+
 	"github.com/NethermindEth/sedge/cli/actions"
 	"github.com/NethermindEth/sedge/configs"
-	"github.com/NethermindEth/sedge/internal/pkg/commands"
-	log "github.com/sirupsen/logrus"
+	"github.com/NethermindEth/sedge/internal/utils"
+	cmpcli "github.com/compose-spec/compose-go/cli"
 	"github.com/spf13/cobra"
 )
 
-func RunCmd(cmdRunner commands.CommandRunner, sedgeActions actions.SedgeActions) *cobra.Command {
+func RunCmd(sedgeActions actions.SedgeActions) *cobra.Command {
 	// Flags
 	var (
 		generationPath string
@@ -34,31 +40,135 @@ func RunCmd(cmdRunner commands.CommandRunner, sedgeActions actions.SedgeActions)
 		Use:   "run [flags]",
 		Short: "Run services",
 		Long:  "Run all the generated services",
-		Run: func(cmd *cobra.Command, args []string) {
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			var found bool
+
+			if err := filepath.Walk(generationPath, func(path string, info os.FileInfo, err error) error {
+				if info.IsDir() {
+					return nil
+				}
+				if info.Name() == configs.DefaultDockerComposeScriptName {
+					found = true
+					// Check if provided docker-compose script is a valid docker-compose script
+					if err := validateCompose(path); err != nil {
+						return fmt.Errorf(configs.InvalidComposeErr, err)
+					}
+
+					// Parse provided docker-compose script
+					// cmp, err := parseCompose(path)
+					// if err != nil {
+					// 	return err
+					// }
+
+					// Check if provided services are valid
+					if len(*services) > 0 {
+						actualServices, err := loadServices(path)
+						if err != nil {
+							return err
+						}
+						for _, service := range *services {
+							if !utils.Contains(actualServices, service) {
+								return fmt.Errorf(configs.InvalidService, service)
+							}
+						}
+					}
+				}
+				return nil
+			}); err != nil {
+				return fmt.Errorf(configs.GenPathErr, err)
+			}
+
+			if !found {
+				return fmt.Errorf(configs.ComposeNotFoundErr, generationPath)
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := sedgeActions.ManageDependencies(actions.ManageDependenciesOptions{
 				Dependencies: []string{"docker"},
 				Install:      true,
 			}); err != nil {
-				log.Fatalf("dependency error: %s", err.Error())
+				return fmt.Errorf(configs.DependencyErr, err)
 			}
 			err := sedgeActions.SetupContainers(actions.SetupContainersOptions{
 				GenerationPath: generationPath,
 				Services:       *services,
 			})
 			if err != nil {
-				log.Fatalf("error setting up service containers: %s", err.Error())
+				return fmt.Errorf(configs.SetupContainersErr, err)
 			}
 			err = sedgeActions.RunContainers(actions.RunContainersOptions{
 				GenerationPath: generationPath,
 				Services:       *services,
 			})
 			if err != nil {
-				log.Fatalf("error starting service containers: %s", err.Error())
+				return fmt.Errorf(configs.StartingContainersErr, err)
 			}
+
+			return nil
 		},
 	}
 
 	cmd.Flags().StringVarP(&generationPath, "path", "p", configs.DefaultAbsSedgeDataPath, "generation path for sedge data")
 	services = cmd.Flags().StringSlice("services", []string{}, "List of services to run. If this flag is not provided, all services will run.")
 	return cmd
+}
+
+// Leave it commented for now, we might need it in the future
+// parseCompose parse a Sedge valid docker-compose script
+// func parseCompose(path string) (*generate.ComposeData, error) {
+// 	// try to load compose file
+// 	composeBytes, err := os.ReadFile(path)
+// 	if err != nil {
+// 		return nil, fmt.Errorf(configs.ReadingYmlErr, err)
+// 	}
+// 	var composeData generate.ComposeData
+// 	err = yaml.Unmarshal(composeBytes, &composeData)
+// 	if err != nil {
+// 		return nil, fmt.Errorf(configs.ParsingYmlErr, err)
+// 	}
+// 	return &composeData, nil
+// }
+
+// loadServices get the services from a Sedge valid docker-compose script
+func loadServices(path string) ([]string, error) {
+	composeBytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf(configs.ReadingYmlErr, err)
+	}
+	var composeData map[string]interface{}
+	// It was already parsed by parseCompose
+	_ = yaml.Unmarshal(composeBytes, &composeData)
+
+	for topLevelK, field := range composeData {
+		if topLevelK == "services" {
+			services, ok := field.(map[string]interface{})
+			if !ok {
+				return nil, fmt.Errorf(configs.ParsingServicesErr)
+			}
+			var servicesNames []string
+			for service := range services {
+				servicesNames = append(servicesNames, service)
+			}
+			return servicesNames, nil
+		}
+	}
+
+	return nil, fmt.Errorf(configs.ServicesNotFoundErr)
+}
+
+// validateCompose validate a docker-compose script according to the docker-compose specification
+func validateCompose(path string) error {
+	// Get current working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf(configs.GetPWDError, err)
+	}
+	// Check if provided docker-compose script is a valid script according to the docker-compose specification
+	_, err = cmpcli.ProjectFromOptions(&cmpcli.ProjectOptions{
+		WorkingDir:  wd,
+		ConfigPaths: []string{path},
+		Environment: make(map[string]string)})
+	return err
 }
--- go.mod
+++ go.mod
@@ -31,7 +31,9 @@ require (
 require (
 	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/compose-spec/compose-go v1.10.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/distribution/distribution/v3 v3.0.0-20221103125252-ebfa2a0ac0a9 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
@@ -40,11 +42,13 @@ require (
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/holiman/uint256 v1.2.1 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kilic/bls12-381 v0.1.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
@@ -56,6 +60,9 @@ require (
 	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/wealdtech/go-bytesutil v1.2.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
