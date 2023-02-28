2ac8815b12f204fd3607aca9a06aad2dce7380a3
AntiD2ta <mtpotro41@gmail.com>
2023-02-28 18:09:51 +0000

    format: utils.go from utils pkg

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -233,7 +233,7 @@ func UriValidator(input []string) (string, bool) {
 		}
 	}
 	return "", true
-} //TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
+} // TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
 
 // ENodesValidator validates a list of EL boot nodes and returns an error if any
 // of them is invalid.
@@ -249,7 +249,7 @@ func ENodesValidator(bootNodes []string) error {
 		set[bootNode] = struct{}{}
 	}
 	return nil
-} //TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
+} // TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
 
 // ENRValidator validates a list of CL boot nodes and returns an error if any
 // of them is invalid.
@@ -265,4 +265,4 @@ func ENRValidator(bootNodes []string) error {
 		set[bootNode] = struct{}{}
 	}
 	return nil
-} //TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
+} // TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
