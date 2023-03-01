1d6d949cbeb8ea8b8eaa22993a44f8458137681b
Miguel Tenorio <46824157+AntiD2ta@users.noreply.github.com>
2023-03-01 12:50:32 -0500

    test: URL validators unit tests (#257)
    * test: Add unit tests for UriValidator
    
    * test: Add unit tests for ENodesValidator
    
    * test: Add unit tests for ENRValidator

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -233,7 +233,7 @@ func UriValidator(input []string) (string, bool) {
 		}
 	}
 	return "", true
-} // TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
+}
 
 // ENodesValidator validates a list of EL boot nodes and returns an error if any
 // of them is invalid.
@@ -244,12 +244,12 @@ func ENodesValidator(bootNodes []string) error {
 			return fmt.Errorf("%s: %s", configs.ErrDuplicatedBootNode, bootNode)
 		}
 		if !regexEnode.MatchString(bootNode) {
-			return fmt.Errorf(configs.InvalidEnode, bootNode)
+			return fmt.Errorf(configs.InvalidEnodeError, bootNode)
 		}
 		set[bootNode] = struct{}{}
 	}
 	return nil
-} // TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
+}
 
 // ENRValidator validates a list of CL boot nodes and returns an error if any
 // of them is invalid.
@@ -260,9 +260,9 @@ func ENRValidator(bootNodes []string) error {
 			return fmt.Errorf("%s: %s", configs.ErrDuplicatedBootNode, bootNode)
 		}
 		if !regexEnr.MatchString(bootNode) {
-			return fmt.Errorf(configs.InvalidEnr, bootNode)
+			return fmt.Errorf(configs.InvalidEnrError, bootNode)
 		}
 		set[bootNode] = struct{}{}
 	}
 	return nil
-} // TODO: Add tests to avoid regression (partially covered by Generate cmd tests with good test cases)
+}
