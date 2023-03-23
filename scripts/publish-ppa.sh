34180a65e417670e714de17fa2b97bca41e0177f
Carlos Bermudez Porto <43155355+cbermudez97@users.noreply.github.com>
2023-03-22 23:06:52 -0400

    fix: Windows release workflow (#267)
    * fix: remove unnecessary steps to install docker in windows release job
    
    * fix: wrong variable for version
    
    * fix: update publish ppa script
    
    * fix: install mockgen on publish ppa script
    
    * fix: wrong version tag in publish ppa script

--- scripts/publish-ppa.sh
+++ scripts/publish-ppa.sh
@@ -2,14 +2,17 @@
 #exit when any command fails
 set -e
 cd /home/runner/work/sedge/sedge/sedge
+go install github.com/golang/mock/mockgen@v1.6.0
+go generate ./...
 mkdir -p build/package/debian/src/github.com/NethermindEth/sedge/
 rsync -aq . build/package/debian/src/github.com/NethermindEth/sedge/ --exclude build/ --exclude .git/ --exclude docs/
 cd build/package/debian/src/github.com/NethermindEth/sedge/ && go mod vendor
 cd /home/runner/work/sedge/sedge/sedge
 
-echo "sedge ($VERSION) jammy; urgency=medium
+export SVERSION=${VERSION#v}
+echo "sedge ($SVERSION) jammy; urgency=medium
 
-  * Sedge ($VERSION release)
+  * Sedge ($SVERSION release)
 
  -- Nethermind <devops@nethermind.io>  $( date -R )" > /home/runner/work/sedge/sedge/sedge/build/package/debian/debian/changelog
 
@@ -17,9 +20,9 @@ cd build/package/debian
 debuild -S -uc -us
 cd ..
 echo 'Signing package'
-debsign -p 'gpg --batch --yes --no-tty --pinentry-mode loopback --passphrase-file /tmp/PASSPHRASE' -S -k$PPA_GPG_KEYID sedge_${VERSION}_source.changes
+debsign -p 'gpg --batch --yes --no-tty --pinentry-mode loopback --passphrase-file /tmp/PASSPHRASE' -S -k$PPA_GPG_KEYID sedge_${SVERSION}_source.changes
 echo 'Uploading'
-dput -f ppa:nethermindeth/sedge sedge_${VERSION}_source.changes
+dput -f ppa:nethermindeth/sedge sedge_${SVERSION}_source.changes
 echo "Publishing Sedge to PPA complete"
 echo 'Cleanup'
-rm -r sedge_$VERSION*
+rm -r sedge_$SVERSION*
