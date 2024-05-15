c162255d7965c8e7b7d62f17b9ce36b5207ed92d
Haneen Khalifa <haneenhany1205@gmail.com>
2024-05-15 15:13:48 +0300

    test: add test case in TestSupportMEVBoost

--- configs/init.go
+++ configs/init.go
@@ -73,7 +73,7 @@ var networksConfigs map[string]NetworkConfig = map[string]NetworkConfig{
 		Name:               NetworkHolesky,
 		NetworkService:     "merge",
 		GenesisForkVersion: "0x00017000",
-		SupportsMEVBoost:   false,
+		SupportsMEVBoost:   true,
 		CheckpointSyncURL:  "https://checkpoint-sync.holesky.ethpandaops.io/",
 		RelayURLs: []string{
 			"https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@boost-relay-holesky.flashbots.net",
