515788bec3b477403c491f0590aec6f816d3f0b2
Haneen Khalifa <haneenhany1205@gmail.com>
2024-06-18 07:16:16 +0300

    style: adjust formatting

--- lido/contracts/mevboost_relay_list/MEVBoostRelayAllowedList_test.go
+++ lido/contracts/mevboost_relay_list/MEVBoostRelayAllowedList_test.go
@@ -35,7 +35,7 @@ func TestMEVBoostRelayAllowedListSuite(t *testing.T) {
 func (s *MEVBoostRelayAllowedListTestSuite) SetupSuite() {
 	// Silence logger
 	log.SetOutput(io.Discard)
-	
+
 	key, err := crypto.GenerateKey()
 	if err != nil {
 		s.T().Fatalf("Failed to generate private key: %v", err)
@@ -59,7 +59,7 @@ func (s *MEVBoostRelayAllowedListTestSuite) SetupSuite() {
 	s.callOpts = &bind.CallOpts{
 		Pending: false,
 		From:    s.address,
-		Context: nil, 
+		Context: nil,
 	}
 
 	contractAddress, tx, apiContract, err := DeployApi(s.auth, s.sim.Client(), s.address)
@@ -104,7 +104,7 @@ func (s *MEVBoostRelayAllowedListTestSuite) TestSetAndGetManager() {
 	}
 	if managerAddress != newManagerAddress {
 		s.T().Errorf("Incorrect manager address. Expected: %s, Got: %s", newManagerAddress.Hex(), managerAddress.Hex())
-	} 
+	}
 }
 
 func (s *MEVBoostRelayAllowedListTestSuite) TestDismissManager() {
@@ -122,7 +122,7 @@ func (s *MEVBoostRelayAllowedListTestSuite) TestDismissManager() {
 	}
 	s.sim.Commit()
 
-	address, _:= s.apiContract.GetManager(s.callOpts)
+	address, _ := s.apiContract.GetManager(s.callOpts)
 	if address != common.BigToAddress(big.NewInt(0)) {
 		s.T().Fatalf("Expected GetManager to return address(0) after dismissing manager. Expected: %s, Got: %s", common.BigToAddress(big.NewInt(0)).Hex(), address.Hex())
 	}
@@ -146,8 +146,8 @@ func (s *MEVBoostRelayAllowedListTestSuite) TestGetRelaysAmount() {
 
 func (s *MEVBoostRelayAllowedListTestSuite) TestGetRelayByUri() {
 	expectedRelay := Struct0{
-		Uri: "example_uri",
-		Operator: "example_operator",
+		Uri:         "example_uri",
+		Operator:    "example_operator",
 		IsMandatory: true,
 		Description: "example_description",
 	}
@@ -156,25 +156,25 @@ func (s *MEVBoostRelayAllowedListTestSuite) TestGetRelayByUri() {
 		s.T().Fatalf("Failed to call AddRelay: %v", err)
 	}
 	s.sim.Commit()
-	
+
 	relay, err := s.apiContract.GetRelayByUri(s.callOpts, expectedRelay.Uri)
 	if err != nil {
 		s.T().Fatalf("Failed to call GetRelayByUri: %v", err)
 	}
 	if !reflect.DeepEqual(relay, expectedRelay) {
 		s.T().Fatalf("Returned relay does not match expected data. Expected: %+v, Got: %+v", expectedRelay, relay)
-	}	
+	}
 }
 
 func (s *MEVBoostRelayAllowedListTestSuite) TestGetAllowedListVersion() {
 	version, err := s.apiContract.GetAllowedListVersion(s.callOpts)
-	if err != nil{
+	if err != nil {
 		s.T().Fatalf("Failed to call GetAllowedListVersion: %v", err)
 	}
 
 	addedRelay := Struct0{
-		Uri: "example__uri",
-		Operator: "example_op",
+		Uri:         "example__uri",
+		Operator:    "example_op",
 		IsMandatory: true,
 		Description: "example",
 	}
@@ -185,10 +185,10 @@ func (s *MEVBoostRelayAllowedListTestSuite) TestGetAllowedListVersion() {
 	s.sim.Commit()
 
 	updatedVersion, err := s.apiContract.GetAllowedListVersion(s.callOpts)
-	if err != nil{
+	if err != nil {
 		s.T().Fatalf("Failed to call GetAllowedListVersion: %v", err)
 	}
-	expectedVersion := version.Add(big.NewInt(1),version)
+	expectedVersion := version.Add(big.NewInt(1), version)
 	if updatedVersion.Cmp(expectedVersion) != 0 {
 		s.T().Errorf("Incorrect relays amount. Expected: %s, Got: %s", expectedVersion, updatedVersion)
 	}
