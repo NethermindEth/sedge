c4a016e06100327e86b76b3fae498bf3b1d3aa87
sugh01 <19183308+sugh01@users.noreply.github.com>
2024-06-19 00:49:00 +0200

    updates to Teku key import

--- internal/images/validator-import/teku/context/validator-init.sh
+++ internal/images/validator-import/teku/context/validator-init.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 set -e
 
 if [[ "$(ls /data/keys)" ]]; then
@@ -8,11 +7,19 @@ else
     cd /keystore/validator_keys
     for key in *; do
         FILENAME=`echo ${key} | sed 's/.json//g'`
-        cp ${key} "/data/keys/${FILENAME}.json"
-        cp ../keystore_password.txt "/data/passwords/${FILENAME}.txt"
+        cp "$key" "/data/keys/${FILENAME}.json"
+        
+        # Check if password file exists for the key
+        if [[ -f "../${FILENAME}.txt" ]]; then
+            cp "../${FILENAME}.txt" "/data/passwords/${FILENAME}.txt"
+        else
+            # Use default password file
+            cp "../keystore_password.txt" "/data/passwords/${FILENAME}.txt"
+        fi
+        
         echo "Copying ${key}"
     done
 fi
 
-# ensure teku access for new keys
+# Ensure teku access for new keys
 chmod -R 777 /data
