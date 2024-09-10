1cbe69518e4e773f090967425cc45cf3f73d62b5
Haneen Khalifa <haneenhany1205@gmail.com>
2024-09-10 17:29:49 +0300

    feat: Update service versions

--- internal/monitoring/services/grafana/dotenv.go
+++ internal/monitoring/services/grafana/dotenv.go
@@ -16,7 +16,7 @@ limitations under the License.
 package grafana
 
 var dotEnv map[string]string = map[string]string{
-	"GRAFANA_IMAGE":          "grafana/grafana-oss:latest",
+	"GRAFANA_IMAGE":          "grafana/grafana-oss:9.4.3",
 	"GRAFANA_PORT":           "3000",
 	"GRAFANA_ADMIN_USER":     "admin",
 	"GRAFANA_ADMIN_PASSWORD": "admin",
