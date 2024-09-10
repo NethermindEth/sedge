7dbf8abfd71978083a646166442e2a6819e67820
Haneen Khalifa <haneenhany1205@gmail.com>
2024-09-10 14:57:39 +0300

    feat: Update services images

--- internal/monitoring/services/node_exporter/dotenv.go
+++ internal/monitoring/services/node_exporter/dotenv.go
@@ -16,6 +16,6 @@ limitations under the License.
 package node_exporter
 
 var dotEnv map[string]string = map[string]string{
-	"NODE_EXPORTER_IMAGE": "prom/node-exporter:v1.1.2",
+	"NODE_EXPORTER_IMAGE": "prom/node-exporter:latest",
 	"NODE_EXPORTER_PORT":  "9100",
 }
