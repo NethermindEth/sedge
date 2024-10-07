5f547d72a8751abd58d3f83665e066e4bf876d24
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-10-07 18:05:49 +0100

    docs: fix broken links in landing page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styled from '@emotion/styled';
 import { ArrowUpRight as LinkIcon, BookOpen, HelpCircle, Info } from 'react-feather';
+import SearchBar from '@theme/SearchBar';
 import {QuickLinks} from "../components/QuickLinks";
 import {SupportedNetworks} from "../components/SupportedNetworks";
 import {Header} from "../components/Header";
