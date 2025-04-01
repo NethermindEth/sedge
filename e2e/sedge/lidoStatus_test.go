2e56a01718f7c039fd0045fab2b8182d88e1037c
Haneen Hany <124837763+khalifaa55@users.noreply.github.com>
2025-04-01 16:27:57 +0200

    tests: Add tests for lido rewards on hoodi (#510)
    * tests: Add tests for lido rewards on hoodi
    
    * fix: Update tests
    
    * fix: Update lido-status tests
    
    * fix: Update tests

--- e2e/sedge/lidoStatus_test.go
+++ e2e/sedge/lidoStatus_test.go
@@ -232,25 +232,48 @@ func TestE2E_LidoStatus_RewardAddressNotFound_Mainnet(t *testing.T) {
 	e2eTest.run()
 }
 
-// func TestE2E_LidoStatus_ValidNodeID_Hoodi(t *testing.T) {
-// 	// Test context
-// 	var (
-// 		runErr error
-// 	)
-// 	// Build test case
-// 	e2eTest := newE2ESedgeTestCase(
-// 		t,
-// 		// Arrange
-// 		nil,
-// 		// Act
-// 		func(t *testing.T, binaryPath string, dataDirPath string) {
-// 			runErr = base.RunCommand(t, binaryPath, "sedge", "lido-status", "--nodeID", "1", "--l", "--network", "hoodi")
-// 		},
-// 		// Assert
-// 		func(t *testing.T, dataDirPath string) {
-// 			assert.NoError(t, runErr, "lido status command should succeed with the given arguments")
-// 		},
-// 	)
-// 	// Run test case
-// 	e2eTest.run()
-// }
+func TestE2E_LidoStatus_ValidNodeID_Hoodi(t *testing.T) {
+	// Test context
+	var (
+		runErr error
+	)
+	// Build test case
+	e2eTest := newE2ESedgeTestCase(
+		t,
+		// Arrange
+		nil,
+		// Act
+		func(t *testing.T, binaryPath string, dataDirPath string) {
+			runErr = base.RunCommand(t, binaryPath, "sedge", "lido-status", "--nodeID", "5", "--l", "--network", "hoodi")
+		},
+		// Assert
+		func(t *testing.T, dataDirPath string) {
+			assert.NoError(t, runErr, "lido status command should succeed with the given arguments")
+		},
+	)
+	// Run test case
+	e2eTest.run()
+}
+
+func TestE2E_LidoStatus_InvalidRewardAddress_Hoodi(t *testing.T) {
+	// Test context
+	var (
+		runErr error
+	)
+	// Build test case
+	e2eTest := newE2ESedgeTestCase(
+		t,
+		// Arrange
+		nil,
+		// Act
+		func(t *testing.T, binaryPath string, dataDirPath string) {
+			runErr = base.RunCommand(t, binaryPath, "sedge", "lido-status", "0xccbhk45", "--network", "hoodi")
+		},
+		// Assert
+		func(t *testing.T, dataDirPath string) {
+			assert.Error(t, runErr, "lido status command should fail")
+		},
+	)
+	// Run test case
+	e2eTest.run()
+}
