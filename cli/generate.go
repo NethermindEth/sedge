6688049dcb0384fbecec994baba1c13d38771196
Haneen Hany <haneenhany1205@gmail.com>
2025-11-12 17:39:29 +0200

    fix: duplicate lines

--- cli/generate.go
+++ cli/generate.go
@@ -127,9 +127,7 @@ You can generate:
 	cmd.AddCommand(ValidatorSubCmd(sedgeAction))
 	cmd.AddCommand(MevBoostSubCmd(sedgeAction))
 	cmd.AddCommand(OpFullNodeSubCmd(sedgeAction))
-	cmd.AddCommand(OpFullNodeSubCmd(sedgeAction))
 
-	cmd.PersistentFlags().BoolVar(&lidoNode, "lido", false, "generate Lido CSM node")
 	cmd.PersistentFlags().BoolVar(&lidoNode, "lido", false, "generate Lido CSM node")
 	cmd.PersistentFlags().StringVarP(&generationPath, "path", "p", configs.DefaultAbsSedgeDataPath, "generation path for sedge data. Default is sedge-data")
 	cmd.PersistentFlags().StringVarP(&network, "network", "n", "mainnet", "Target network. e.g. mainnet,sepolia, hoodi, gnosis, chiado, etc.")
@@ -624,4 +622,3 @@ func nodeType() string {
 	}
 	return nodeType
 }
-
